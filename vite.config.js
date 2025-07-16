536322b5665f005a1d85c7b2c3bd39099220478e
ZainAhmed <104525206+ZainAhmed9@users.noreply.github.com>
2025-07-17 00:20:57 +0500

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // IMPORTANT for Vite routing
+  base: '/RedBasket---Organic-Grocery-Store-React-Vite-/', // IMPORTANT for Vite routing
 });
